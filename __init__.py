d1658daf55a9b3286dc03ff43175130d834de7cd
Wille Kantelus <willecode@gmail.com>
2022-03-20 18:21:28 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,33 @@
+if "bpy" in locals():
+    # Runs if add-ons are being reloaded from blender
+    import importlib
+    importlib.reload(alternate_move_operator)
+    print('Alternate move addon reloaded modules')
+else:         
+    from . import alternate_move_operator
+    print('Alternate move addon imported modules')
+
+import bpy
+bl_info = {
+    "name": "Alternate move operator",
+    "author": "Wilkan",
+    "version": (1, 0, 0),
+    "blender": (3, 0, 0),
+    "location": "View3D",
+    "description": "Move active object along ",
+    "category": "Development",
+}
+
+def register():
+    bpy.utils.register_class(alternate_move_operator.OBJECT_OT_alternate_move)
+    print("Alternate move operator registered")
+    pass              
+
+def unregister():
+    bpy.utils.unregister_class(alternate_move_operator.OBJECT_OT_alternate_move)
+    print("Alternate move operator unregistered")
+
+    pass              
+            
+if __name__  == '__main__':
+    register()
---
+++ alternate_move_operator.py
@@ -0,0 +1,100 @@
+from pickle import FLOAT, OBJ
+import bpy
+import gpu
+from gpu_extras.batch import batch_for_shader
+import numpy as np
+from bpy.props import FloatVectorProperty, FloatProperty
+import mathutils
+
+
+def draw_callback(self, context):
+    shader = gpu.shader.from_builtin('3D_UNIFORM_COLOR')
+    batch = batch_for_shader(shader, 'POINTS', {"pos": self.coords})
+    shader.bind()
+    shader.uniform_float("color", (0, 1, 0, 1))
+    view_proj = bpy.context.region_data.perspective_matrix
+    shader.uniform_float("ModelViewProjectionMatrix", view_proj @ self.model)
+    batch.draw(shader)
+
+class OBJECT_OT_alternate_move(bpy.types.Operator):
+    """Move an object with the mouse"""
+    bl_idname = "object.alternate_move"
+    bl_label = "Alternate Move Operator"
+    move_fac = 0.01
+    obj: bpy.types.Object
+    coords = []
+    model: mathutils.Matrix
+    view: mathutils.Matrix
+    first_mouse_xy = []
+
+    preview_pos: FloatProperty(name="Preview Position", default=0)
+    # ****************************
+
+    def modal(self, context, event):
+        mouse_x_offset = event.mouse_x - self.first_mouse_xy[0]
+        mouse_y_offset = event.mouse_y - self.first_mouse_xy[1]
+
+        camera_right_vec = np.array(self.view[0][:3])
+        camera_up_vec = np.array(self.view[1][:3])
+
+        view_inv = np.linalg.inv(self.view)
+        camera_pos = np.array([view_inv[0][3], view_inv[1][3], view_inv[2][3]])
+        camera_dist = np.linalg.norm(camera_pos)
+        move_fac = camera_dist * 0.001
+
+        context.area.tag_redraw() # Force area redraw
+        
+        if event.type == 'MOUSEMOVE':
+            matrix_world = self.obj.matrix_world.copy()
+
+            obj_pos = [
+                matrix_world[0][3],
+                matrix_world[1][3],
+                matrix_world[2][3]
+            ]
+
+            obj_pos += (camera_right_vec * mouse_x_offset * move_fac)
+            obj_pos += (camera_up_vec * mouse_y_offset * move_fac)
+
+            matrix_world[0][3] = obj_pos[0]
+            matrix_world[1][3] = obj_pos[1]
+            matrix_world[2][3] = obj_pos[2]
+
+            self.model = matrix_world
+
+        elif event.type == 'LEFTMOUSE':
+            bpy.context.object.matrix_world = self.model
+            bpy.types.SpaceView3D.draw_handler_remove(self.handle, 'WINDOW')
+            return {'FINISHED'}
+
+        elif event.type in {'RIGHTMOUSE', 'ESC'}:
+            bpy.types.SpaceView3D.draw_handler_remove(self.handle, 'WINDOW')
+            return {'CANCELLED'}
+
+        return {'RUNNING_MODAL'}
+
+    def invoke(self, context, event):
+        self.view = context.region_data.view_matrix
+        self.first_mouse_xy = [event.mouse_x, event.mouse_y]
+        if context.object and context.area.type == 'VIEW_3D':
+            self.obj = context.object
+            self.processObjMesh()
+
+            # Add the drawing callback function to view3D region
+            # This will run every time the region is drawn
+            args = (self, context)
+            self.handle = bpy.types.SpaceView3D.draw_handler_add(draw_callback,
+             args, 'WINDOW', 'POST_VIEW')
+            context.window_manager.modal_handler_add(self)
+            return {'RUNNING_MODAL'}
+        else:
+            self.report({'WARNING'}, "No active object, could not finish")
+            return {'CANCELLED'}
+    
+    def processObjMesh(self):
+        self.coords = []
+        verts = bpy.context.object.data.vertices.values()
+        for v in verts:
+            self.coords.append(v.co)
+
+
