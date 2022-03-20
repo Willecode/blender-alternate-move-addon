fc83bbba5d443359023bf98541a0be3989711837
Wille Kantelus <willecode@gmail.com>
2022-03-20 18:55:05 +0200

    preview color change based on active material base color

--- alternate_move_operator.py
+++ alternate_move_operator.py
@@ -5,13 +5,13 @@ from gpu_extras.batch import batch_for_shader
 import numpy as np
 from bpy.props import FloatVectorProperty, FloatProperty
 import mathutils
-
+from mathutils import Color
 
 def draw_callback(self, context):
     shader = gpu.shader.from_builtin('3D_UNIFORM_COLOR')
     batch = batch_for_shader(shader, 'POINTS', {"pos": self.coords})
     shader.bind()
-    shader.uniform_float("color", (0, 1, 0, 1))
+    shader.uniform_float("color", self.preview_color)
     view_proj = bpy.context.region_data.perspective_matrix
     shader.uniform_float("ModelViewProjectionMatrix", view_proj @ self.model)
     batch.draw(shader)
@@ -26,6 +26,7 @@ class OBJECT_OT_alternate_move(bpy.types.Operator):
     model: mathutils.Matrix
     view: mathutils.Matrix
     first_mouse_xy = []
+    preview_color = (0, 1, 0, 1)
 
     preview_pos: FloatProperty(name="Preview Position", default=0)
     # ****************************
@@ -41,7 +42,7 @@ class OBJECT_OT_alternate_move(bpy.types.Operator):
         camera_pos = np.array([view_inv[0][3], view_inv[1][3], view_inv[2][3]])
         camera_dist = np.linalg.norm(camera_pos)
         move_fac = camera_dist * 0.001
-
+        
         context.area.tag_redraw() # Force area redraw
         
         if event.type == 'MOUSEMOVE':
@@ -74,10 +75,17 @@ class OBJECT_OT_alternate_move(bpy.types.Operator):
         return {'RUNNING_MODAL'}
 
     def invoke(self, context, event):
-        self.view = context.region_data.view_matrix
-        self.first_mouse_xy = [event.mouse_x, event.mouse_y]
         if context.object and context.area.type == 'VIEW_3D':
+            self.view = context.region_data.view_matrix
+            self.first_mouse_xy = [event.mouse_x, event.mouse_y]
             self.obj = context.object
+
+            if self.obj.active_material is not None:
+                nodes = self.obj.active_material.node_tree.nodes
+                principled = next(n for n in nodes if n.type == 'BSDF_PRINCIPLED')
+                base_color = principled.inputs['Base Color']
+                value = base_color.default_value
+                self.preview_color = (value[0], value[1], value[2], 1)
             self.processObjMesh()
 
             # Add the drawing callback function to view3D region
